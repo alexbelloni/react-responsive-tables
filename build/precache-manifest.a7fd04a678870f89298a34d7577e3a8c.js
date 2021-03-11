f410b7ed747570e43330c3b774539fdad79c125f
alexbelloni <alexbelloni@outlook.com>
2021-03-11 11:25:11 -0500

    added build folder to create the demo page

---
+++ build/precache-manifest.a7fd04a678870f89298a34d7577e3a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75234db27915b7b1abb60cbb3e715813",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "002d255b532710208af9",
+    "url": "/build/static/css/2.326c04ff.chunk.css"
+  },
+  {
+    "revision": "523979eb829343e5976c",
+    "url": "/build/static/css/main.39d70b72.chunk.css"
+  },
+  {
+    "revision": "002d255b532710208af9",
+    "url": "/build/static/js/2.f44b2271.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.f44b2271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523979eb829343e5976c",
+    "url": "/build/static/js/main.9b6017c7.chunk.js"
+  },
+  {
+    "revision": "6c4be330a7b4aa246b44",
+    "url": "/build/static/js/runtime-main.049686fa.js"
+  }
+]);
